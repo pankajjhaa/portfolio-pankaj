bdd03bf5dc7b2dce01e329b5157bc9621793259b
pankaj <jhap0384@gmail.com>
2024-02-09 18:23:48 +0530

    INitial commit

--- components/sections/experience.tsx
+++ components/sections/experience.tsx
@@ -9,10 +9,11 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
+import {useTheme} from "next-themes";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
-
+    const { theme } = useTheme()
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
@@ -21,22 +22,24 @@ export default function Experience() {
           <React.Fragment key={index}>
               <VerticalTimelineElement
                   contentStyle={{
+                      background:
+                          theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                       boxShadow: "none",
                       border: "1px solid rgba(0, 0, 0, 0.05)",
                       textAlign: "left",
                       padding: "1.3rem 2rem",
                   }}
                   contentArrowStyle={{
-                      // borderRight:
-                      //   theme === "light"
-                      //     ? "0.4rem solid #9ca3af"
-                      //     : "0.4rem solid rgba(255, 255, 255, 0.5)",
+                      borderRight:
+                        theme === "light"
+                          ? "0.4rem solid #9ca3af"
+                          : "0.4rem solid rgba(255, 255, 255, 0.5)",
                   }}
                   date={item.date}
                   icon={item.icon}
                   iconStyle={{
-                      // background:
-                      //   theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                      background:
+                        theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
                       fontSize: "1.5rem",
                   }}
               >
