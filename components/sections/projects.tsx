5293faa6d41e5095d1c41bd76508f8bd1df52198
pankaj <jhap0384@gmail.com>
2024-02-13 10:43:09 +0530

    Project design fix

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -2,26 +2,21 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { projectsData } from "@/lib/data";
+import {projectsData} from "@/lib/data";
 import Project from "./project";
-import { useSectionInView } from "@/lib/hooks";
+import {useSectionInView} from "@/lib/hooks";
 
 export default function Projects() {
-  const { ref } = useSectionInView("Projects", 0.5);
+    const {ref} = useSectionInView("Projects", 0.5);
 
-  return (
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-      <SectionHeading>My projects</SectionHeading>
-      <div className={"grid grid-cols-1 md:grid-cols-3 container gap-5"}>
-        {projectsData.map((project, index) => (
-          <React.Fragment key={index}>
-              <div >
-                  <Project {...project} />
-              </div>
-
-          </React.Fragment>
-        ))}
-      </div>
-    </section>
-  );
+    return (<section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+            <SectionHeading>My projects</SectionHeading>
+            <div className={"grid grid-cols-1 md:grid-cols-3 container gap-5"}>
+                {projectsData.map((project, index) => (<React.Fragment key={index}>
+                        <div>
+                            <Project {...project} />
+                        </div>
+                    </React.Fragment>))}
+            </div>
+        </section>);
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,7 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
 import foodnfeedimg from "@/public/projectsImage/foodnfeed.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
