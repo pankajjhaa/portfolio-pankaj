0f51a1684226e02f8d53513770a77f1e57ba44da
pankaj <jhap0384@gmail.com>
2024-08-08 14:29:46 +0530

    Project design fix

--- components/sections/intro.tsx
+++ components/sections/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Pankaj Jha.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">3.5 years</span> of experience. I enjoy
+        <span className="font-bold">4 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
